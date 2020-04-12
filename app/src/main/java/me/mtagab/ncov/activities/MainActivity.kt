00c1f77cdc7bb6c464efa17050684b4693ae2a52
Mark Tagab <proxyshadow@gmail.com>
2020-04-12 13:32:39 +0800

    GUI update

--- app/src/main/java/me/mtagab/ncov/activities/MainActivity.kt
+++ app/src/main/java/me/mtagab/ncov/activities/MainActivity.kt
@@ -3,49 +3,33 @@ package me.mtagab.ncov.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.GoogleApiClient
 import me.mtagab.ncov.R
 
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "health_main"
-    private var googleApiClient: GoogleApiClient? = null
-    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var signInButton: SignInButton = findViewById(R.id.sign_in_button)
-        val main = findViewById<TextView>(R.id.application_title)
+        val login = findViewById<Button>(R.id.login)
+        val register = findViewById<Button>(R.id.register)
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("714385865679-q6ljhpl52l3g6f9st08otqmuui9pdhkm.apps.googleusercontent.com")
-            .requestEmail()
-            .requestProfile()
-            .build()
+        val username = findViewById<TextView>(R.id.username)
+        val password = findViewById<TextView>(R.id.password)
 
-        googleApiClient = GoogleApiClient.Builder(this)
-            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-            .build()
-
-        signInButton.setOnClickListener {
-            val intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
-            startActivityForResult(intent, RC_SIGN_IN)
+        login.setOnClickListener {
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
         }
 
-        main.setOnClickListener {
-            val intent = Intent(this, UserActivity::class.java)
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
@@ -72,23 +56,5 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)
-
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-                // ...
-            }
-        }
-    }
 
 }
--- app/src/main/java/me/mtagab/ncov/fragments/health.kt
+++ app/src/main/java/me/mtagab/ncov/fragments/health.kt
@@ -15,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [travel.newInstance] factory method to
+ * Use the [health.newInstance] factory method to
  * create an instance of this fragment.
  */
-class travel : Fragment() {
+class health : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +36,7 @@ class travel : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_travel, container, false)
+        return inflater.inflate(R.layout.fragment_health, container, false)
     }
 
     companion object {
@@ -46,12 +46,12 @@ class travel : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment travel.
+         * @return A new instance of fragment health.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            travel().apply {
+            health().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
